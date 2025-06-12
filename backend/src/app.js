1b57dc0a195f05c165ab4ce5b0d19426453fad47
Kaushik Kumar <kaushikkumar@Kaushiks-MacBook-Pro.local>
2025-06-12 13:19:56 +0530

    added config file and serve build from express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,33 +1,30 @@
-import express from 'express'
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './db/dbConnect.js';
-import cookieParser from 'cookie-parser';
-import authRouter from './routes/authRouter.js';
-
-// for enabling the read from env file
-dotenv.config();
-
-// initializing the app
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan"; // Import morgan
+import connectDB from "./db/dbConnect.js";
+import authRouter from "./routes/authRouter.js";
+import { config } from "./config.js";
 const app = express();
 
-// middlewares
-app.use(cors())
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(cors());
+app.use(morgan("dev")); // Add morgan here for request logging
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-connectDB()
+connectDB();
 
-// routes
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 
-const PORT = process.env.PORT || 3000
+app.use(express.static(path.join(__dirname, "../../frontend/built")));
 
-app.get('/', (req, res) => {
-    res.send("HELLO WORLD");
-})
+app.get("/*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/built/index.html"));
+});
 
-// listening all the request on given PORT
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT : ${PORT}`);
-})
+app.listen(config.PORT, () => console.log(`Server on PORT: ${config.PORT}`));
